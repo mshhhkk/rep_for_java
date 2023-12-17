855c908c9dc5439a2f60c42c46566c39bcd06403
mshhhkk <lady.shkurnaya@gmail.com>
2023-12-17 21:40:34 +0300

    lab3

---
+++ lab3/src/infa.java
@@ -0,0 +1,10 @@
+public class infa {
+    private final String surname = "Шкурная";
+    private final int group = 6;
+
+    @Override
+    public String toString() {
+        return "Фамилия: " + surname + " " +
+                ", группа:" + group;
+    }
+}
